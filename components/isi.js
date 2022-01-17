e415ab50f133fb385f6ce4c8c824e6160e8e4da6
Vyan4033 <vyanc403@gmail.com>
2022-01-18 01:25:38 +0700

    commit

--- components/isi.js
+++ components/isi.js
@@ -5,7 +5,7 @@ const Isi = (props) => {
         <div className="container">
             <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
                 <div className="col-md-6 p-lg-6 mx-auto my-5 py-5">
-                    <h1 className="display-5 fw-bold text-primary d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                    <h1 className="display-5 fw-bold text-primary d-flex flex-wrap justify-content-center py-5 my-5 mb-4 border-bottom">
                             {props.judul}
                     </h1>
                     <p className="lead fw-bold ">{props.caption}</p>
