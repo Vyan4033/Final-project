bf043297af4f445f9cc69e686e9e845150c74657
Vyan4033 <vyanc403@gmail.com>
2022-01-18 01:23:43 +0700

    commit

--- components/isi.js
+++ components/isi.js
@@ -4,7 +4,7 @@ const Isi = (props) => {
     return(
         <div className="container">
             <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
-                <div className="col-md-6 p-lg-6 mx-auto my-2">
+                <div className="col-md-6 p-lg-6 mx-auto my-5 py-5">
                     <h1 className="display-5 fw-bold text-primary d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                             {props.judul}
                     </h1>
